ebad1ee3976cc6fb017e4888501c4ad251a366b4
yuen1318 <yuen.yalung@gmail.com>
2017-10-03 06:09:44 -0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,12 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/materialize/css/materialize.min.css">
+    <link rel="stylesheet" href="/css/fa/css/font-awesome.min.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    <h4>Materialize Madafaka</h4>
-    <div id="items">
+<body class="blue-grey lighten-5">
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m12 l12">
+
+            <table class="bordered centered responsive-table striped">
+                <thead>
+                    <tr>
+                    <th class="hide">ID</th>
+                    <th>Text</th>
+                    <th>Body</th>
+                    <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+                <!--end of thead-->
+
+                <tbody id="tbl_items"></tbody>
+                <!--end of tbody-->
+            </table>
+            <!--end of table-->
+
+
+            </div>
+        </div>
     </div>
 </body>
 <script src="/js/jquery/jquery.min.js"></script>
@@ -26,9 +48,15 @@ $(document).ready(function() {
             var output="";
 
             for(i=0; i<data.length; i++){
-                output += "<li>" + data[i].text +" "+ data[i].body;
+
+                output += "<tr>"+ 
+                        "<td class='hide'>"+ data[i].id +"</td>"+
+                        "<td>"+ data[i].text +"</td>"+
+                        "<td>"+ data[i].body +"</td>"+
+                        "<td><a class='fa fa-lg fa-pencil' href='/api/items" +data[i].id+ "'></a></td>"+
+                        "<td><a class='fa fa-lg fa-trash' href='/api/items" +data[i].id+ "'></a></td>";
             }
-            $("#items").append(output);
+            $("#tbl_items").append(output);
             
         }
     });
